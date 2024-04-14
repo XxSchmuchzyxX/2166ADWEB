d0b287caf101f3b385ec61f2e2ec5aeff74db8fb
XxSchmuchzyxX <123358104+XxSchmuchzyxX@users.noreply.github.com>
2024-04-15 02:38:16 +0800

    Add files via upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,8 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
     declarations: [AppComponent]
   }));
 
@@ -14,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FinalActivity1'`, () => {
+  it(`should have as title 'booksapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FinalActivity1');
+    expect(app.title).toEqual('booksapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('FinalActivity1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('booksapp app is running!');
   });
 });
